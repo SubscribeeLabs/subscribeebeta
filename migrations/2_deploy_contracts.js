a6cc5ef90efdedecfde85d4e5f6a78f879e33203
SubscribeeLabs <info@subscribee.org>
2022-04-26 14:56:39 -0500

    remove cache to respect gitignore

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const Subscribee = artifacts.require("Subscribee.sol");
-const Beehive = artifacts.require("Beehive.sol");
-const Token = artifacts.require("MockToken.sol");
-
-const Web3 = require('web3');
-
-module.exports = async function(deployer, _network, accounts) {
-};
